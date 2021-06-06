d96c3cca557b94896ec2a8166427f8ad4c5f1ca7
saluma23 <33370484+saluma23@users.noreply.github.com>
2021-06-06 23:02:40 +0530

    Add files via upload

---
+++ developer-forum/src/main/java/com/salman/controller/EmployeeServlet.java
@@ -0,0 +1,62 @@
+package com.salman.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.salman.dao.Employeedao;
+import com.salman.model.Employee;
+
+@WebServlet("/register")
+public class EmployeeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private Employeedao employeeDao = new Employeedao();
+
+    
+    public EmployeeServlet()
+    {
+    	super();
+    }
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    	    throws ServletException, IOException {
+    	response.getWriter().append("Served at: ").append(request.getContextPath());
+    	
+    	RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/views/employeeregister.jsp");
+    	dispatcher.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String address = request.getParameter("address");
+        String contact = request.getParameter("contact");
+
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setUsername(username);
+        employee.setPassword(password);
+        employee.setContact(contact);
+        employee.setAddress(address);
+
+        try {
+            employeeDao.registerEmployee(employee);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        RequestDispatcher dispatcher=request.getRequestDispatcher("/WEB-INF/views/employeedetails.jsp");
+    	dispatcher.forward(request, response);
+    }
+}
